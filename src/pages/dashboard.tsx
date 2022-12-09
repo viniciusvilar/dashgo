dfc48307a9a0d11ade958c21b2656b1fa05bd448
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2022-12-09 08:39:17 -0300

    Configurando gráfico

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -27,6 +27,24 @@ const options = {
     tooltip: {
         enabled: false,
     },
+    xaxis: {
+        type: 'datetime',
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks: {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            '2022-03-18T00:00:00:00.000Z',
+            '2022-03-19T00:00:00:00.000Z',
+            '2022-03-20T00:00:00:00.000Z',
+            '2022-03-21T00:00:00:00.000Z',
+            '2022-03-22T00:00:00:00.000Z',
+            '2022-03-23T00:00:00:00.000Z',
+            '2022-03-24T00:00:00:00.000Z',
+        ],
+    },
 };
 
 const series = [
