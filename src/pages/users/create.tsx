6b28bcf8f6343f5c5e7abf0f91159264e81532ef
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2022-12-12 10:10:43 -0300

    Colocando links

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack} from "
 import { Input } from "../../components/Form/Input";
 import { Sidebar } from "../../components/Sidebar/index";
 import { Header } from "../../components/Header";
+import Link from "next/link";
 
 export default function CreateUser() {
     return (
@@ -32,7 +33,9 @@ export default function CreateUser() {
 
                     <Flex mt='8' justify='flex-end'>
                         <HStack spacing='4'>
-                            <Button colorScheme='whiteAlpha'>Cancelar</Button>
+                            <Link href='/users' passHref>
+                                <Button as='a' colorScheme='whiteAlpha'>Cancelar</Button>
+                            </Link>
                             <Button colorScheme='pink'>Salvar</Button>
                         </HStack>
                     </Flex>
