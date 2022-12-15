1e4e7af4c9eb993d2b1930473aad0fb6e13f87ae
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2022-12-15 09:18:59 -0300

    Logic pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,9 +5,12 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error } = useUsers()
+    const [page, setPage] = useState(1)
+
+    const { data, isLoading, isFetching, error } = useUsers(page)
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -64,7 +67,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data?.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={['4', '4', '6']}>
@@ -84,9 +87,9 @@ export default function UserList() {
                             </Table>
 
                             <Pagination
-                                totalCountOfRegisters={200}
-                                currentPage={4}
-                                onPageChange={() => {}}
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
                             />
                         </>
                     )}
